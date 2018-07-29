7033066384c17d529129b2d9fca4c772df90cfad
fabuss254 <fabuss254@outlook.fr>
2018-07-29 21:18:14 +0200

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -24,7 +24,7 @@ bot.on("ready", function(){
 });
 
 function Refresh(){
-  http.get('https://search.roblox.com/catalog/json?SortType=3&SortType3&ResultsPerPage=1', (res) => {
+  http.get('http://search.roblox.com/catalog/json?SortType=3&SortType3&ResultsPerPage=1', (res) => {
     const { statusCode } = res;
     const contentType = res.headers['content-type'];
 
