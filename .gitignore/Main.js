b5842beb7e247c6fcbe8a0662752c4db5a277eb6
fabuss254 <fabuss254@outlook.fr>
2018-07-29 21:16:11 +0200

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -6,7 +6,7 @@ const MaximumPrice = -1; //Is the bot only notify item with x maximum of robux (
 const ShirtAndPants = false; //Is the bot listen to pants and shirt in addition of others limiteds?
 const LiveCounter = true; //Is the bot edit the message with the number of remaining item (only limitedU)
 const LiveCounterTimeout = 30; //When do the bot stop editing the message after x seconds (Need LiveCounter to true)
-const RefreshRate = 5; //How much time to wait before getting items
+const RefreshRate = 10; //How much time to wait before getting items
 
 // Main script
 
@@ -17,14 +17,45 @@ var CurrentItem
 
 bot.on("ready", function(){
   console.log("Bot is ready to use!");
-  bot.user.setPresence({game:{name: "Notifying "+ bot.guilds.array.length +" servers", url: "https://www.twitch.tv/fabuss255", type: 1}});
+  bot.user.setPresence({game:{name: "On "+ bot.guilds.array().length +" servers", url: "https://www.twitch.tv/fabuss255", type: 1}});
   
   Refresh()
   bot.setInterval(Refresh, RefreshRate*1000);
 });
 
 function Refresh(){
-  
+  http.get('https://search.roblox.com/catalog/json?SortType=3&SortType3&ResultsPerPage=1', (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }
+    if (error) {
+      console.error(error.message);
+      res.resume();
+      return;
+    }
+
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        bot.channels.findAll('name', 'roblox-catalog').map(channel => channel.send("Nom d'item: " + parsedData.Name));
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error at GET http: ${e.message}`);
+  });
 };
 
 bot.login(process.env.TOKEN);
