af32e8aa313fe22b492d3c3b285aa17c69a10c52
fabuss254 <fabuss254@outlook.fr>
2018-07-29 21:50:04 +0200

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -49,9 +49,10 @@ function Refresh(){
       try {
         const parsedData = JSON.parse(rawData);
         var NewItemEmbed = new Discord.RichEmbed()
-          .setTitle("NEW ITEM!")
+          .setTitle("Nouvelle item ou item mis a jour!")
           .setDescription("AssetId: "+ parsedData[0].AssetId + "\nName: " + parsedData[0].Name + "\nDescription: " + parsedData[0].Description)
           .setImage(parsedData[0].ThumbnailUrl);
+          .setURL(parsedData[0].AbsoluteUrl)
         bot.channels.findAll('name', 'roblox-catalog').map(channel => channel.send(NewItemEmbed));
       } catch (e) {
         console.error(e.message);
